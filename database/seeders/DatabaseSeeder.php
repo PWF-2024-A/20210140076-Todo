8d0d459e894b18984dcca8b0da34c17731b80729
Sofi Miranda <sofimiranda559@gmail.com>
2024-05-24 23:23:01 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,29 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name'=> 'Admin',
+            'email'=> 'admin@admin.com',
+            'is_admin'=> true,
+        ]);
+        User::factory()->create([
+            'name'=> 'Sofi Miranda',
+            'email'=> 'sofimiranda559@gmail.com',
+            'is_admin'=> true,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       User::factory(100)->create();
+       Todo::factory(500)->create();
     }
 }
